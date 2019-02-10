0d328937218c0ea32e8d6e499bc08952b38d9b92
Ankur Nigam <ankur.nigam198@gmail.com>
2019-02-11 03:24:03 +0530

    formatting

--- IdentifierFSM/program.cpp
+++ IdentifierFSM/program.cpp
@@ -6,78 +6,74 @@ using namespace std;
 
 class IdentifierFSM
 {
-    bool CheckFirstCharacter(char d)
-    {
-        if ((d >= 'a' && d <= 'z') ||
-            (d >= 'A' && d <= 'Z') ||
-            d == '_')
-        {
-            return true;
-        }
-        return false;
-    }
+	bool CheckFirstCharacter(char d)
+	{
+		if ((d >= 'a' && d <= 'z') ||
+			(d >= 'A' && d <= 'Z') ||
+			d == '_')
+		{
+			return true;
+		}
+		return false;
+	}
 
-    bool isNumber(char d)
-    {
-        if (d >= '0' && d <= '9')
-        {
-            return true;
-        }
-        return false;
-    }
+	bool isNumber(char d)
+	{
+		if (d >= '0' && d <= '9')
+		{
+			return true;
+		}
+		return false;
+	}
 
-  public:
-    void DifferentiateTokens(string input)
-    {
-        list<string> tokens;
+	bool isStringStarted = false;
+	bool isString(char d)
+	{
+		if (d == '\"')
+		{
+			isStringStarted = !isStringStarted;
+		}
+	}
 
-        string tempToken;
+public:
+	void DifferentiateTokens(string input)
+	{
+		string tempToken;
 
-        bool isStringStarted = false;
-        //it should also have rules for comments, and single quotes
+		for (int i = 0; i < input.length(); i++)
+		{
+			char t = input[i];
 
-        for (int i = 0; i < input.length(); i++)
-        {
-            char t = input[i];
+			if (isString(t))
+			{
+				continue;
+			}
 
-            if (t == '\"')
-            {
-                isStringStarted = !isStringStarted;
-            }
-
-            if (isStringStarted)
-            {
-                continue;
-            }
-
-            if (tempToken.length() > 0)
-            {
-                if (CheckFirstCharacter(t) || isNumber(t))
-                {
-                    tempToken += t;
-                }
-                else
-                {
-                    cout << tempToken << ",";
-
-                    tokens.push_back(tempToken.substr(0));
-
-                    tempToken.clear();
-                }
-            }
-            else
-            {
-                if (CheckFirstCharacter(t))
-                {
-                    tempToken += t;
-                }
-            }
-        }
-    }
+			if (tempToken.length() > 0)
+			{
+				if (CheckFirstCharacter(t) || isNumber(t))
+				{
+					tempToken += t;
+				}
+				else
+				{
+					cout << tempToken << ",";
+					tempToken.clear();
+				}
+			}
+			else
+			{
+				if (CheckFirstCharacter(t))
+				{
+					tempToken += t;
+				}
+			}
+		}
+	}
 };
 
 int main()
 {
-    IdentifierFSM fsm;
-    fsm.DifferentiateTokens(string("int a; void b=a+5;string x=\"aishwarya\""));
+	IdentifierFSM fsm;
+	fsm.DifferentiateTokens(string("int a; void b=a+5;string x=\"aishwarya\""));
 }
